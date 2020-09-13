567a5c01c7822a07e6f9ba65cfea4126e3702499
Marc Pawlowsky <marcpawl@gmail.com>
2020-09-13 16:05:41 -0400

    Turn on link time optimization

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -17,7 +17,7 @@ endif()
 # Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
+option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
 
 if(ENABLE_IPO)
   include(CheckIPOSupported)
